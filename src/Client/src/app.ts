0358a2e8fef287876ec91cf123e73b482f753b31
SimonNyvall <simon.k.nyvall@proton.me>
2025-02-18 21:48:45 +0100

    chore: improve UI

--- src/Client/src/app.ts
+++ src/Client/src/app.ts
@@ -1,12 +1,28 @@
+import { Dropdown } from "./common/dropdown/dropdown";
 import { RequestEntity } from "./entities/request-entity";
 import { TabRequest } from "./request/tab-request";
 
 export class App {
     static dependencies = [
         TabRequest,
+        Dropdown
     ];
 
     public request: RequestEntity = new RequestEntity();
+
+    public httpMethods: Array<{ label: string, value: string, colorClass: string }> = [
+        { label: 'GET', value: 'GET', colorClass: 'color-get' },
+        { label: 'POST', value: 'POST', colorClass: 'color-post' },
+        { label: 'PUT', value: 'PUT', colorClass: 'color-put' },
+        { label: 'DELETE', value: 'DELETE', colorClass: 'color-delete' },
+        { label: 'PATCH', value: 'PATCH', colorClass: 'color-patch' }
+    ];
+    public type: string = 'GET'; 
+
+    public requestName: string = 'New Request';
+    public isEditingName: boolean = false;
+    public requestNameInput: HTMLInputElement;
+
     private slidingPanel: HTMLElement;
     private resizeHandle: HTMLElement;
     private isResizing: boolean = false;
@@ -24,6 +40,22 @@ export class App {
         this.request.headers.filter((header) => header.key !== '' && header.value !== '');
 
         console.log(this.request);
+    }
+
+    public editRequestName(): void {
+        this.isEditingName = true;
+        setTimeout(() => {
+            this.requestNameInput.focus();
+        }, 0);
+    }
+
+    public saveRequestName(): void {
+        this.isEditingName = false;
+    }
+
+    public selectText(event: FocusEvent): void {
+        const input = event.target as HTMLInputElement;
+        input.select();
     }
 
     private onMouseDown(event: MouseEvent): void {
